03b744b7fe2c740085c9916451a90bae4bc1a65e
Thakur Pradeep rawat <P.s.rawat4458@gmail.com>
2026-05-26 17:12:37 +0530

    Add indexes of auth

---
+++ auth/indexes/auth_indexes.sql
@@ -0,0 +1,15 @@
+﻿CREATE INDEX IX_Users_Phone
+ON [auth].[Users]([Phone]);
+GO
+
+CREATE INDEX IX_UserAddresses_UserId
+ON [auth].[UserAddresses]([UserId]);
+GO
+
+CREATE INDEX IX_UserRoles_UserId
+ON [auth].[UserRoles]([UserId]);
+GO
+
+CREATE INDEX IX_RefreshTokens_UserId
+ON [auth].[RefreshTokens]([UserId]);
+GO
